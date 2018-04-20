47429a1231b3fc4e920a7cb187668b3bf1421be0
Aditia Dika <aditiaaadicka@gmail.com>
2018-04-20 17:46:36 +0700

    Displaying user fotoprofile info & install noty

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -9,14 +9,31 @@
                         {{ $users->name }} 's Profile
                     </p>
                 </div>
-                <div class="panel panel-body">
+                <div class="panel-body">
                     <center>
                         <img src="{{ Storage::url($users->avatar) }}" alt="" width="70px" height="70px" style="border-radius: 50px;">
                     </center>
+                    <br>
+                    <p class="text-center">
+                        {{ $users->profile->location }}
+                    </p>
                     <p class="text-center">
                         @if(Auth::id() == $users->id)
-                            <a href="{{ route('profile.edit') }}" class="btn btn-lg btn-info">Edit Your Profile</a>
+                            <a href="{{ route('profile.edit') }}" class="btn btn-sm btn-info">Edit Your Profile</a>
                         @endif
+                    </p>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <p class="text-center">
+                        About me
+                    </p>
+                </div>
+                <div class="panel-body">
+                    <p class="text-center">
+                        {{ $users->profile->about }}
                     </p>
                 </div>
             </div>
