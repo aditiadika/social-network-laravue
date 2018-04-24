ed518a72d7585f82b0a827ddbe985f9aa273d4c7
Aditia Dika <aditiaaadicka@gmail.com>
2018-04-24 12:25:32 +0700

    lesson 13 user pending friend request

--- app/Traits/Friendable.php
+++ app/Traits/Friendable.php
@@ -45,7 +45,7 @@ trait Friendable
                         ->get();
 
         foreach ($f1 as $friendship):
-            array_push($friends, User::find($friendship->user_requester));
+            array_push($friends, User::find($friendship->user_requested));
         endforeach;
 
         $friends2 = array();
@@ -55,9 +55,39 @@ trait Friendable
                         ->get();
 
         foreach ($f2 as $friendship):
-            array_push($friends2, User::find($friendship->user_requester));
+            array_push($friends2, User::find($friendship->requester));
         endforeach;
 
         return array_merge($friends, $friends2);
+    }
+
+    public function pending_friend_requests()
+    {
+        $users = array();
+
+        $friendships = Friendship::where('status', 0)
+            ->where('user_requested', $this->id)
+            ->get();
+
+        foreach ($friendships as $friendship):
+            array_push($users, User::find($friendship->requester));
+        endforeach;
+
+        return $users;
+    }
+
+    public function friends_ids()
+    {
+        return collect($this->friends())->pluck('id');
+    }
+
+    public function is_friends_with($user_id)
+    {
+        if (in_array($user_id, $this->friends_ids()->toArray()))
+        {
+            return response()->json('true', 200);
+        } else {
+            return response()->json('false', 200);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,27 @@ Route::get('/', function () {
 });
 
 Route::get('/add', function () {
-    return \App\User::find(4)->add_friend(1);
+    return \App\User::find(1)->add_friend(3);
 });
 
 Route::get('/accept', function () {
-    return \App\User::find(1)->accept_friend(4);
+    return \App\User::find(4)->accept_friend(1);
+});
+
+Route::get('/pending', function () {
+    return \App\User::find(3)->pending_friend_requests();
+});
+
+Route::get('/ids', function () {
+    return \App\User::find(1)->friends_ids();
+});
+
+Route::get('/is', function () {
+    return \App\User::find(1)->is_friends_with(3);
 });
 
 Route::get('/friends', function () {
-    return \App\User::findOrFail(2)->friends();
+    return \App\User::findOrFail(1)->friends();
 });
 
 Auth::routes();
@@ -35,6 +47,4 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('/profile/{slug}', 'ProfileController@index')->name('profile');
     Route::get('/profile/edit/profile', 'ProfileController@edit')->name('profile.edit');
     Route::post('/profile/update/profile', 'ProfileController@update')->name('profile.update');
-
-
 });
